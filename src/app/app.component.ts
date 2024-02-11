551350943db0cb477fc5a6b0149134a9de8e9abc
nic0q <nicolas.farfan.ch@usach.cl>
2024-02-10 23:05:05 -0300

    [feat]: Communitacio beetween components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,27 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CardComponent } from './card/card.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CardComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'app';
+  title = 'NICO';
+  public ene:number = 0
+  sum():void{
+    this.ene+=1
+  }
+  public lista:number[] = [1,2,3,4]
+
+  games = [
+    {
+      title: "Hello",
+      phrase: "good",
+      url: "https"
+    }
+  ]
 }
